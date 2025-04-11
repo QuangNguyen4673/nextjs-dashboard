7eed3ee1ccd74414d8ed56c3cce5c656000768e0
quangnguyen <quangnguyenmail96@gmail.com>
2025-04-12 00:30:03 +0700

    remove export in route.ts

--- app/query/route.ts
+++ app/query/route.ts
@@ -4,7 +4,7 @@ const sql = postgres(process.env.POSTGRES_URL!, {
   ssl: 'require',
 })
 
-export async function listInvoices() {
+async function listInvoices() {
   const data = await sql`
      SELECT invoices.amount, customers.name
      FROM invoices
